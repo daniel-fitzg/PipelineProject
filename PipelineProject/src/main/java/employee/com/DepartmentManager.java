1bdc121560f82a37f0f9b3b8a3ee1e88cf3a7825
dshirt <g00351835@gmit.ie>
2020-04-25 12:49:12 +0100

    Test commit as I cannot get a merge request to work

---
+++ PipelineProject/src/main/java/employee/com/DepartmentManager.java
@@ -0,0 +1,9 @@
+package employee.com;
+
+public class DepartmentManager extends Manager {
+
+    @Override
+    public double calculatePayment() {
+        return 0.0;
+    }
+}
---
+++ PipelineProject/src/main/java/employee/com/Director.java
@@ -0,0 +1,9 @@
+package employee.com;
+
+public class Director extends Manager {
+
+    @Override
+    public double calculatePayment() {
+        return 0.0;
+    }
+}
