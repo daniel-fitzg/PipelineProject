ac6c1d405c2e21ac3fa5ad5bb7d0f4f8a5c40472
daniel-fitzg <dan8790@icloud.com>
2020-04-29 14:21:54 +0100

    Changes methods return type

--- src/test/java/EmployeeFactory.java
+++ src/test/java/EmployeeFactory.java
@@ -1,6 +1,5 @@
 import employee.com.DepartmentManager;
 import employee.com.Director;
-import employee.com.Employee;
 
 /*
 * Used for testing purposes - returns Employee objects rather than manual entry for the test
@@ -8,12 +7,12 @@ import employee.com.Employee;
 
 public class EmployeeFactory {
 
-    static Employee getValidDirector() {
+    static Director getValidDirector() {
         return new Director("0001", "Jacob O'Leary", "Galway", "21/04/1978",
                 "4858697A", 120000, "Dublin");
     }
 
-    static Employee getValidDepartmentManager() {
+    static DepartmentManager getValidDepartmentManager() {
         return new DepartmentManager("0002", "Mike Lam", "Wexford", "01/02/1983",
                 "1426794W", 43000, "Grocery");
     }
